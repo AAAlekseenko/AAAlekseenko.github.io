36b2469862a2f1ea906556b376a01cbb157238e0
AAAlekseenko <53532473+AAAlekseenko@users.noreply.github.com>
2020-11-24 21:42:14 +0300

    final 3 block and 60%last block
    Fix  title in first block.
    Need fix slider in last block

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 
-
 function initMenu(){
     let menu = document.querySelector('.js-menu');
     let btnMenu = document.querySelector('.js-menu-btn');
@@ -9,3 +8,28 @@ function initMenu(){
 };
 
 initMenu();
+$('.jq-slider').slick({
+    infinite: true,
+    dots:true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+        {
+          breakpoint: 993,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+            
+          },
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false
+          }
+        }
+      
+      ]
+  });
